d2a765db8f2b247e1d3e045b33c42d5627e6ed83
Gianluca Cerritelli <gianluca.cerritelli@gmail.com>
2017-06-26 07:53:04 +0100

    new philatelic item form

--- ProjectSazan/ProjectSazan.Web/PhilatelyViewModels/Models/PhilatelicItemViewModel.cs
+++ ProjectSazan/ProjectSazan.Web/PhilatelyViewModels/Models/PhilatelicItemViewModel.cs
@@ -1,11 +1,20 @@
-﻿using System.ComponentModel.DataAnnotations;
+﻿using System;
+using System.ComponentModel.DataAnnotations;
 
 namespace ProjectSazan.Web.Models.PhilatelyViewModels
 {
     public class PhilatelicItemViewModel
     {
+        public string Catalogue { get; set; }
         [Required]
+        public string Area { get; set; }
         public string Number { get; set; }
+        public int Year { get; set; }
+        [Required]
         public string Description { get; set; }
+        public string Condition { get; set; }
+        public decimal Price { get; set; }
+        public string Currency { get; set; }
+        public DateTime Acquired { get; set; } 
     }
 }
